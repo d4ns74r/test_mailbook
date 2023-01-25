e1c398c8eca975c17971b60e284e0d11a5154879
D4NS74R <97897939+d4ns74r@users.noreply.github.com>
2023-01-25 18:27:36 +0300

    Выделена фикстура. Переход на Pytest

---
+++ contact.py
@@ -0,0 +1,9 @@
+
+class Contact:
+    def __init__(self, header, firstname, lastname, number, street):
+        self.header = header
+        self.firstname = firstname
+        self.lastname = lastname
+        self.number = number
+        self.street = street
+
---
+++ test_add_contact.py
@@ -0,0 +1,22 @@
+from contact import Contact
+import pytest
+from application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_addcustomer(app):
+    app.Login(email="denisprokofyev88@gmail.com", password="Probka98")
+    app.Add_contact(Contact(header="Denis", firstname="Denis", lastname="Prokofyev", number="79255712456", street="Petrozavodskaya"))
+    app.Logout()
+
+
+def test_addcustomer_one_symbol(app):
+    app.Login(email="denisprokofyev88@gmail.com", password="Probka98")
+    app.Add_contact(Contact(header="D", firstname="D", lastname="P", number="7", street="P"))
+    app.Logout()
